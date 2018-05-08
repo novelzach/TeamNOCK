e18b13b5da63d8e75980363f9a8af79b9c75ed76
rothbacherc <rothbacherc@gmail.com>
2018-05-08 15:24:26 -0700

    Added basic App framework

--- NOCK_Files/App.js
+++ NOCK_Files/App.js
@@ -1,8 +1,27 @@
 "use strict";
 exports.__esModule = true;
+var express = require("express");
+var logger = require("morgan");
+var bodyParser = require("body-parser");
 var App = /** @class */ (function () {
     function App() {
+        this.expressApp = express();
+        this.middleware(); // builds express instance
+        this.routes(); //builds routes for the service
+        this.idGenerator = 100;
+        //initialize models here
     }
+    App.prototype.middleware = function () {
+        this.expressApp.use(logger('dev'));
+        this.expressApp.use(bodyParser.json());
+        this.expressApp.use(bodyParser.urlencoded({ extended: false }));
+    };
+    App.prototype.routes = function () {
+        var router = express.Router();
+        //router.get here(need paths first)
+        //router.post here(need paths and models first)
+        //expressApp calls here (need above routes first)
+    };
     return App;
 }());
 exports.App = App;
--- NOCK_Files/App.ts
+++ NOCK_Files/App.ts
@@ -10,7 +10,31 @@ import {DataAccess} from './DataAccess';
 
 class App{
     public expressApp: express.Application;
+    //declare models here
+    public idGenerator:number;
 
+    constructor(){
+	this.expressApp = express();
+	this.middleware(); // builds express instance
+	this.routes(); //builds routes for the service
+	this.idGenerator = 100;
+	//initialize models here
+    }
+
+    private middleware(): void {
+	this.expressApp.use(logger('dev'));
+	this.expressApp.use(bodyParser.json());
+	this.expressApp.use(bodyParser.urlencoded({ extended: false}));
+    }
+
+    private routes(): void {
+	let router = express.Router();
+	//router.get here(need paths first)
+
+	//router.post here(need paths and models first)
+
+	//expressApp calls here (need above routes first)
+    }
 }
 
 export {App};
