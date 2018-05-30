525a523d9647f370bf15b000486a98785fd77725
novelzach <brooksz@seattleu.edu>
2018-05-29 17:26:48 -0700

    updated testSingle

--- nockApp/Mocha/testSingle.js
+++ nockApp/Mocha/testSingle.js
@@ -12,6 +12,14 @@ var http = require(http);
 chai.use(chaiHttp);
 
 describe('Test GET, single object', function() {
+
+    it('should return a single user when called', function(done) {
+        http.get('http://localhost:8080/app/user/3', function(res) {
+            expect(Response).to.have.status;
+            
+        });
+    });
+
     it('should return user Charlie for userId 3', function(done) {
         http.get('http://localhost:8080/app/user/3', function(res) {
             assert.equal('rothbacher', res.lname);
