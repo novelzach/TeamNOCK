65ecbabd2e4cbb51e59d542bf6855154b8763d02
Jigar Mistry <Jigar@Jigars-MacBook-Pro.local>
2018-06-09 02:02:37 -0700

    mongodb deployed to mlab

--- nockApp/DataAccess.js
+++ nockApp/DataAccess.js
@@ -2,6 +2,7 @@
 exports.__esModule = true;
 var Mongoose = require("mongoose");
 var DataAccess = /** @class */ (function () {
+    //static DB_CONNECTION_STRING:string = 'mongodb://dbAdmin:test@localhost:3000/nock?authSource=admin';
     //Enter DB link above ^^
     function DataAccess() {
         DataAccess.connect();
@@ -17,7 +18,7 @@ var DataAccess = /** @class */ (function () {
         this.mongooseInstance = Mongoose.connect(this.DB_CONNECTION_STRING);
         return this.mongooseInstance;
     };
-    DataAccess.DB_CONNECTION_STRING = 'mongodb://dbAdmin:test@localhost:3000/nock?authSource=admin';
+    DataAccess.DB_CONNECTION_STRING = 'mongodb://nockuser1:TeamNock123@ds016718.mlab.com:16718/nockdb';
     return DataAccess;
 }());
 exports.DataAccess = DataAccess;
