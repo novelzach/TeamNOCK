5209c3f178e18f1bdec30f65269120e23aa40848
novelzach <brooksz@seattleu.edu>
2018-06-10 14:53:56 -0700

    git is angry

---
+++ NOCK_Files/jsonconversion_broken.ts
@@ -0,0 +1,42 @@
+var isValidElement = function isValidElement(element) {
+  return element.name && element.value;
+};
+
+
+var formToJSON_deconstructed = function formToJSON_deconstructed(elements) {
+
+  var reducerFunction = function reducerFunction(data, element) {
+    data[element.name] = element.valu
+    return data;
+  };
+
+  var reducerInitialValue = {};
+  var formData = [].reduce.call(elements, reducerFunction, reducerInitialValue);
+
+  return formData;
+};
+
+
+var formToJSON = function formToJSON(elements) {
+  return [].reduce.call(elements, function (data, element) {
+    if (isValidElement(element)) {      
+      data[element.name] = element.value;
+    }
+    return data;
+  }, {});
+};
+
+var handleFormSubmit = function handleFormSubmit(event) {
+  event.preventDefault();
+
+  // no idea why this doesn't work...
+  var data = formToJSON(form.elements);
+
+  // For testing only:
+  var dataContainer =  JSON.stringify(data, null, "  ");
+
+  // this is where it would get passed to the post function
+};
+
+var form = document.getElementsByClassName('contact-form')[0];
+form.addEventListener('submit', handleFormSubmit);
